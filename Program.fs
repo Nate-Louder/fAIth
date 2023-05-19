dfdd03db92e3bbe95c5e301df4f7b8eb3fea0f9c
Nate Louder <nlouder@projetech.com>
2023-05-19 13:23:35 -0400

    Interpreter now uses files instead of repl

--- Program.fs
+++ Program.fs
@@ -1,6 +1,38 @@
 ﻿namespace MathInterpreter
 
 open System
+open System.IO
+
+module Helpers = 
+
+    open Parse
+    open Types.BasicOperations
+    open Types.Errors
+    let readFromFile (filePath: string) =
+        try
+            File.ReadAllLines filePath
+        with
+            | :? System.IO.FileNotFoundException ->
+                printfn "File not found: %s" filePath
+                [||]
+            | ex -> 
+                printfn "An error occurred while reading the file: %s" ex.Message
+                [||]
+
+    let splitLine (line: string) =
+        line.Split(" ")
+        |> Array.toList
+    let processFile (filePath: string) =
+        let lines = readFromFile filePath
+        let parsedLines =
+            lines
+            |> Array.toList
+            |> List.fold ( fun list line -> List.append list (splitLine line)) [] 
+            |> parseOperation
+
+        parsedLines
+        |> List.fold operationValidationCheck (Ok [])
+
 
 module main =
     open Types.BasicOperations
@@ -8,33 +40,30 @@ module main =
     open Types.Errors
     open Operation
     open Parse
+    open Helpers
+
 
     [<EntryPoint>]
     let main args =
 
         let mutable state = Ok { NumberStack = []; Variables = []; Functions = [] }
 
-        while true do
-            printf "💲 " |> ignore
-
-            let input =
-                Console.ReadLine().Split(" ")
-                |> Array.toList
-                |> parseOperation
-                |> List.fold operationValidationCheck (Ok [])
-
-            match input with
+        match args with
+        [|filepath|] ->
+            match processFile filepath with
             | Ok validInput ->
                 match List.fold matchElementType state validInput with
-                | Ok x -> state <- Ok x
-                | Error(FailedOperationAttempt(y, x)) ->
-                    state <- Ok x // Sets the stack to the stack created before the error occured.
-                    printfn "Operation %O failed with parameters %A" y x
-                | Error DivideByZero -> printfn "Cannot divide by zero"
+                | Ok x -> 
+                    state <- Ok x
+                    0
                 | Error err -> 
                     printfn "%A" err
                     state <- state
-            | Error e -> printfn "%O" e
+                    1
+            | Error e -> 
+                printfn "%O" e
+                1
+        | _ -> 
+            printfn "Invalid number of arguments. Please provide the file path."
+            1
 
-
-        0
