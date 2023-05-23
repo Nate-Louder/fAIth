fc84edbd43d28b9c2499309a258da7c3a1a54fd2
Nate Louder <nlouder@projetech.com>
2023-05-23 08:58:27 -0400

    Reformatted to work with microservice template.

---
+++ fAITH/Faith.fs
@@ -0,0 +1,53 @@
+﻿namespace Faith
+
+module Helpers = 
+
+    open Parse
+    open Types.State
+    open Operation
+    open System
+
+    let splitLine (line: string) =
+        line.Split(" ")
+        |> Array.toList
+
+    let processInput input =
+       
+        let state = Ok { NumberStack = []; Variables = []; Functions = [] }
+
+        let inputList = 
+            splitLine input
+            |> parseOperation
+            |> List.fold operationValidationCheck (Ok [])
+
+        match inputList with
+        | Ok validInput ->     
+            match List.fold matchElementType state validInput with
+            | Ok returnState -> Ok returnState
+            | Error err -> Error err
+        | Error err -> Error err
+
+    let processInputConsole =
+       
+        let mutable state = Ok { NumberStack = []; Variables = []; Functions = [] }
+        
+        while true do 
+            printf "💲 "
+            
+            match Console.ReadLine() |> splitLine |> parseOperation |> List.fold operationValidationCheck (Ok []) with
+            |Ok validInput ->     
+                match List.fold matchElementType state validInput with
+                | Ok returnState -> state <- Ok returnState
+                | Error err -> printfn "%A" err
+            |Error err -> printfn "%A" err
+
+
+        
+        
+
+module Faith =
+    open Helpers
+
+    let faith input = processInput input 
+    let faithConsole = processInputConsole
+
